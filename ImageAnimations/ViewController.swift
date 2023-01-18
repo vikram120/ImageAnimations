7ffbca80effd3e922e782f5052305f8b42b158d3
vikram <vikramkunwar1404@gmail.com>
2023-01-18 19:02:15 +0530

    Images Transition

--- ImageAnimations/ViewController.swift
+++ ImageAnimations/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var RedFort: UIImageView!
+    
+    
+    @IBOutlet weak var TajMahal: UIImageView!
+    
+    @IBOutlet weak var IndiaGate: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
+    
+    
+    @IBAction func RedFortBtn(_ sender: UIButton) {
+        
+        UIView.transition(with: RedFort, duration: 3.0, options: .transitionFlipFromBottom, animations: {
+            
+            
+        })
+    }
+    
+    
+    @IBAction func TajButton(_ sender: UIButton) {
+        
+        UIView.transition(with: TajMahal , duration: 3.0, options: .transitionCurlUp, animations: {
+            
+            
+            
+        })
+        
+        
+    }
+    
+    @IBAction func IndiaGateBtn(_ sender: UIButton) {
+        
+        UIView.transition(with: IndiaGate, duration: 3.0, options: .transitionFlipFromLeft, animations: {
+            
+            
+            
+        })
+    }
+    
 
 }
 
